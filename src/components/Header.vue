eb4c9261e9a812df7d7a9ee78784b6d3f31b1691
Vladislav <vladgm233223@gmail.com>
2023-08-11 16:13:29 +0300

    create container

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,11 @@ import Navigation from './Navigation.vue';
 </script>
 <template>
     <header class="header">
-        <RouterLink to="/" class="link">
-            <h2 class="header-logo">Logo</h2>
-        </RouterLink>
-        <Navigation />
-    </header>
+        <div class="container">
+            <RouterLink to="/" class="link">
+                <h2 class="header-logo">Logo</h2>
+            </RouterLink>
+            <Navigation />
+        </div>
+        </header>
 </template>
